357bf3c94108db54d7642bc3ef862cc8d35f8bcd
Semih Ocaklı <semih.ocakli@ogr.ksbu.edu.tr>
2023-08-15 18:57:10 +0300

    degisken tanimlamak

---
+++ Javascript/003-Variable/variable.js
@@ -0,0 +1,11 @@
+let API_KEY = "SAKFŞJSAŞKFAŞDKALDSASLKDLAŞSKDLŞAKDŞADASD5AS4D5SA4DA"
+
+console.log(API_KEY)
+
+// firstName -> camelCase  -> değişecek bilgi 
+// first_name -> snake_case -> değişmeyecek bilgi 
+// FirstName -> PascalCase 
+// first-name -> kebab-case
+
+// https://www.robinwieruch.de/javascript-naming-conventions/
+
