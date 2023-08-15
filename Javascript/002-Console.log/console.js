bd318da633780acab9aa5e979f9ba87ce8501be1
Semih Ocaklı <semih.ocakli@ogr.ksbu.edu.tr>
2023-08-15 18:25:55 +0300

    Javascript giriş

---
+++ Javascript/002-Console.log/console.js
@@ -0,0 +1,13 @@
+console.log(console)
+console.error("Hata bilgisini dikkat çekmesi için yazabiliriz!")
+
+const user = { 
+    firstname: "Semih",
+    lastname:  "Ocakli",
+}
+console.log("log:",user)
+
+console.log("Table::::")
+console.table(user)
+
+console.info("Bilgi..")
